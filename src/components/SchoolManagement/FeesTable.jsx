7d41f520da20e507c89634d9cbac68114ab95918
akhi3394 <akhilvemula3394@gmail.com>
2025-07-10 13:32:43 +0530

    feat:school management inner pages

---
+++ src/components/SchoolManagement/FeesTable.jsx
@@ -0,0 +1,236 @@
+
+import React from 'react';
+import { FaEye, FaEdit, FaTrash } from 'react-icons/fa';
+import downArrow from '/downArrow.svg'
+import upArrrow from '/upArrow.svg'
+import EyeIcon from '/EyeIcon.svg'
+import EditIcon from '/EditIcon.svg'
+import DeleteIcon from '/DeleteIcon.svg'
+import ActionIcon from '/ActionIcon.svg'
+import plus from '/plus.svg'
+import filterIcon from '/filterIcon.svg'
+import Export from '/Export.svg'
+import SearchIcon from '/SearchIcon.svg'
+import { useNavigate } from 'react-router-dom';
+
+const data = [
+  {
+    id:1,
+    logo: '',
+    schoolNameCode: 'GVPS-998 Vidhya Vikas Ma...',
+    board: 'CBSE',
+    location: 'Thanjavur Tamil Nadu',
+    admin: 'ID12451 Devon Lane',
+    totalStudents: '1250',
+    status: 'Active'
+  },
+  {
+    id:2,
+    logo: '',
+    schoolNameCode: 'GVPS-998 Vidhya Vikas Ma...',
+    board: 'CBSE',
+    location: 'Thanjavur Tamil Nadu',
+    admin: 'ID12451 Devon Lane',
+    totalStudents: '1250',
+    status: 'Active'
+  },
+  {
+    id:3,
+    logo: '',
+    schoolNameCode: 'GVPS-998 Vidhya Vikas Ma...',
+    board: 'CBSE',
+    location: 'Thanjavur Tamil Nadu',
+    admin: 'ID12451 Devon Lane',
+    totalStudents: '1250',
+    status: 'Active'
+  },
+  {
+    id:4,
+    logo: '',
+    schoolNameCode: 'GVPS-998 Vidhya Vikas Ma...',
+    board: 'CBSE',
+    location: 'Thanjavur Tamil Nadu',
+    admin: 'ID12451 Devon Lane',
+    totalStudents: '1250',
+    status: 'Active'
+  },
+];
+
+const FeesTable = () => {
+    const navigate = useNavigate();
+
+  return (
+    <>
+   
+      <div className="w-full overflow-x-auto bg-white p-4 rounded-[16px] custom-scrollbar border border-[#E7E7E7]">
+        <div className="flex justify-between items-center px-4 py-2 mb-5">
+          <div className="relative w-[500px] h-[40px]">
+            <input
+              type="text"
+              placeholder="Search for id, School Name"
+              className="w-full h-full rounded-[12px] border border-gray-300 pl-4 pr-10 text-sm placeholder-[#949494] focus:outline-none font-jakarta"
+            />
+            <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-sm font-jakarta">
+              <img src={SearchIcon} alt="SearchIcon" className='w-[24px] h-[24px]'/>
+            </span>
+          </div>
+          <span className='flex gap-4 items-center'>
+            <div className="flex gap-2 border border-[#B0B0B0] py-2 px-2 rounded-[12px] items-center">
+              <span className="text-[#000] text-[14px] cursor-pointer font-jakarta">Filter</span>
+              <img src={filterIcon} alt="filter" />
+            </div>
+            <div className="flex gap-2 border border-[#B0B0B0] py-2 px-2 rounded-[12px] items-center">
+              <span className="text-[#000] text-[14px] cursor-pointer font-jakarta">Export</span>
+              <img src={Export} alt="export" />
+            </div>
+          </span>
+        </div>
+        <div className="rounded-[16px] overflow-x-auto custom-scrollbar w-full border border-[#E7E7E7]">
+          <table className="w-full text-left border-collapse min-w-[1200px] table-auto">
+            <thead>
+              <tr className="bg-[#F6F6F6] text-[#454545] text-[14px] h-[65px] font-medium font-jakarta">
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <input
+                    type="checkbox"
+                    className="appearance-none w-5 h-5 border-2 border-[#454545] rounded-md checked:bg-[#454545] checked:border-[#454545] checked:appearance-auto cursor-pointer"
+                  />
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Logo
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    School Name & Code
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Board
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Location
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Assigned Admin
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Total Students
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    Status
+                    <div className="flex flex-col">
+                      <img src={upArrrow} alt="upArrow" />
+                      <img src={downArrow} alt="downArrow" />
+                    </div>
+                  </div>
+                </th>
+                <th className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                  <div className="flex items-center gap-5">
+                    <span>Action</span>
+                    <span>
+                      <img src={ActionIcon} alt="ActionIcon" />
+                    </span>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row, index) => {
+                const [schoolCode, ...schoolNameParts] = row.schoolNameCode.split(' ');
+                const schoolName = schoolNameParts.join(' ');
+
+                const [locationCity, ...locationStateParts] = row.location.split(' ');
+                const locationState = locationStateParts.join(' ');
+
+                const [adminId, ...adminNameParts] = row.admin.split(' ');
+                const adminName = adminNameParts.join(' ');
+
+                return (
+                  <tr key={index} className="h-[60px] bg-white">
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                      <input
+                        type="checkbox"
+                        className="appearance-none w-5 h-5 border-2 border-[#454545] rounded-md checked:bg-[#454545] checked:border-[#454545] checked:appearance-auto cursor-pointer"
+                      />
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                      <div className="w-[40px] h-[40px] bg-[#E7E7E7] rounded" />
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#454545] font-jakarta">
+                      <div className="flex flex-col leading-tight">
+                        <span className="text-[#1A71F6] font-jakarta">{schoolCode}</span>
+                        <span font-jakarta>{schoolName}</span>
+                      </div>
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#454545] font-jakarta">
+                      {row.board}
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#454545] font-jakarta">
+                      <div className="flex flex-col leading-tight">
+                        <span font-jakarta>{locationCity}</span>
+                        <span font-jakarta>{locationState}</span>
+                      </div>
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#454545] font-jakarta">
+                      <div className="flex flex-col leading-tight">
+                        <span className="text-[#1A71F6] font-jakarta">{adminId}</span>
+                        <span font-jakarta>{adminName}</span>
+                      </div>
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#454545] font-jakarta">
+                      {row.totalStudents}
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[14px] text-[#000]">
+                      <span className="px-2 py-1 rounded-full text-[12px] text-[#008000] font-semibold font-jakarta">{row.status}</span>
+                    </td>
+                    <td className="px-4 py-2 border-b border-[#E7E7E7] text-[#454545] font-jakarta">
+                      <div className="flex space-x-2">
+                        <img src={EyeIcon} alt="EyeIcon" className='w-[24px] h-[24px]' onClick={() => navigate(`/school-management/${row.id}`)}/>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FeesTable;
--- src/components/SchoolManagement/SchoolDetails.jsx
+++ src/components/SchoolManagement/SchoolDetails.jsx
@@ -1,65 +1,171 @@
 import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-
+import backIcon from '/BackIcon.svg';
+import BasicInformation from '/BasicInformation.svg';
+import ContactInformation from '/ContactInformation.svg';
+import SchoolLogo from '/SchoolLogo.svg'
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import ClassTable from './ClassTable';
+import FeesTable from './FeesTable';
+import SubscritionTable from './SubscritionTable';
+import Admintable from './Admintable';
+import TabNavigation from './TabNavigation';
 const SchoolDetails = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  // Mock data for demonstration (replace with actual data fetching logic)
-  const school = {
-    id,
-    schoolName: 'Vidhya Vikas Matriculation',
-    schoolCode: 'GVPS-998',
-    board: 'CBSE',
-    city: 'Thanjavur',
-    state: 'Tamil Nadu',
-    adminId: 'ID12451',
-    adminName: 'Devon Lane',
-    totalStudents: '1250',
-    status: 'Active',
+  const handleBack = () => {
+    navigate('/school-management')
+  }
+    const [searchParams, setSearchParams] = useSearchParams();
+
+  const activeTab = searchParams.get('tab') || 'Class';
+
+  const handleTabChange = (tab) => {
+    setSearchParams({ tab });
+  };
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case 'Class':
+        return <ClassTable />;
+      case 'Admin':
+        return <Admintable />;
+      case 'Subsciption & Billing':
+        return <SubscritionTable />;
+      case 'Fees Collection':
+        return <FeesTable />;
+      default:
+        return <ClassTable />;
+    }
   };
 
   return (
-    <div className="p-6 bg-white rounded-[16px] border border-[#E7E7E7]">
-      <h1 className="text-[24px] font-semibold text-[#000000] font-jakarta mb-6">School Details</h1>
-      <div className="grid grid-cols-2 gap-6">
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">School Code</p>
-          <p className="text-[16px] text-[#1A71F6] font-jakarta">{school.schoolCode}</p>
+    <div className="min-h-screen bg-[#F5F6FA] font-sans ">
+      {/* Header */}
+      <div className="flex justify-between items-center mb-6">
+        <div className="flex items-center gap-2">
+          <img src={backIcon} alt="Back" className="w-[24px] h-[24px] cursor-pointer" onClick={handleBack}/>
+          <h1 className="text-lg font-semibold text-[#1B1E25]">School Profile</h1>
         </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">School Name</p>
-          <p className="text-[16px] text-[#454545] font-jakarta">{school.schoolName}</p>
-        </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">Board</p>
-          <p className="text-[16px] text-[#454545] font-jakarta">{school.board}</p>
-        </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">Location</p>
-          <p className="text-[16px] text-[#454545] font-jakarta">{`${school.city}, ${school.state}`}</p>
-        </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">Assigned Admin</p>
-          <p className="text-[16px] text-[#1A71F6] font-jakarta">{school.adminId}</p>
-          <p className="text-[16px] text-[#454545] font-jakarta">{school.adminName}</p>
-        </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">Total Students</p>
-          <p className="text-[16px] text-[#454545] font-jakarta">{school.totalStudents}</p>
-        </div>
-        <div>
-          <p className="text-[14px] text-[#454545] font-jakarta font-semibold">Status</p>
-          <p className="text-[16px] text-[#008000] font-jakarta">{school.status}</p>
-        </div>
-      </div>
-      <div className="flex justify-end mt-6">
-        <button
-          onClick={() => navigate('/school-management')}
-          className="px-5 py-2 rounded-[8px] bg-[#1A71F6] text-white font-jakarta font-bold"
-        >
-          Back to School List
+        <button className="bg-[#1A71F6] hover:bg-[#155CD1] text-white px-4 py-1.5 text-sm font-medium rounded-md">
+          Message
         </button>
+      </div>
+
+      {/* Info Sections */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-5">
+        {/* Basic Information */}
+        <div className="bg-white rounded-lg shadow-sm p-4">
+          <div className="flex justify-between items-center mb-3">
+            <div className="flex items-center gap-2">
+              <img src={BasicInformation} alt="Basic" className="w-5 h-5" />
+              <p className="text-[16px] font-semibold text-[#1B1E25]">Basic Information</p>
+            </div>
+            <img src="/EditIcon.svg" alt="Edit" className="w-4 h-4 cursor-pointer" />
+          </div>
+
+          <div className="bg-[#F5F6FA] rounded-md px-4 py-3 mb-4 text-[#1B1E25] font-medium text-sm flex justify-start items-center">
+            <img
+              src={SchoolLogo}
+              alt="Logo"
+              className="w-[62px] h-[62px]  mr-3 flex justify-start"
+            />
+            <span className="text-[16px] font-semibold">Vidhya Vikas Matric Hr Sec School</span>
+          </div>
+
+          <div className=" space-y-3 px-4">
+            <div className="flex justify-between">
+              <p className='text-[#666666] text-sm'>School Code</p>
+              <p className="font-medium text-[#000000] text-sm">Code001234</p>
+            </div>
+            <div className="flex justify-between">
+              <p className='text-[#666666] text-sm'>School type</p>
+              <p className="font-medium text-[#000000] text-sm">Private School</p>
+            </div>
+            <div className="flex justify-between">
+              <p className='text-[#666666] text-sm'>Board Affiliation</p>
+              <p className="font-medium text-[#000000] text-sm">State Board</p>
+            </div>
+            <div className="pt-3">
+              <p className="font-semibold text-[#333333] text-start text-sm">Class Strength</p>
+              <div className="flex justify-between mt-2">
+                <p className=" text-[#666666] text-sm">Class</p>
+                <p className="font-medium text-[#000000] text-sm">LKG to 12th Std</p>
+              </div>
+              <div className="flex justify-between">
+                <p className="text-[#666666] text-sm">Total Strength</p>
+                <p className="font-medium text-[#000000] text-sm">1256</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Contact Information */}
+        <div className="bg-white rounded-lg shadow-sm p-4">
+          <div className="flex justify-between items-center mb-3">
+            <div className="flex items-center gap-2">
+              <img src={ContactInformation} alt="Contact" className="w-5 h-5" />
+              <p className="text-[16px] font-semibold text-[#1B1E25]">Contact Information</p>
+            </div>
+            <img src="/EditIcon.svg" alt="Edit" className="w-4 h-4 cursor-pointer" />
+          </div>
+          <div className="border-b border-[#E5E7EB] mb-5"></div>
+          <div className="text-sm text-[#1B1E25] space-y-3 px-2 text-start">
+            <p className="font-semibold text-[#333333] text-sm">Primary Contact</p>
+            <div className="space-y-2">
+              <div className="flex justify-between text-sm">
+                <p className="text-[#666666]">Primary Email</p>
+                <p className="text-[#000000] font-medium">Natrajan</p>
+              </div>
+              <div className="flex justify-between text-sm">
+                <p className="text-[#666666]">Alternate Email</p>
+                <p className="text-[#000000] font-medium">+918906576774</p>
+              </div>
+              <div className="flex justify-between text-sm">
+                <p className="text-[#666666]">Phone Number 1</p>
+                <p className="text-[#000000] font-medium">Father</p>
+              </div>
+              <div className="flex justify-between text-sm">
+                <p className="text-[#666666]">Phone Number 2</p>
+                <p className="text-[#000000] font-medium">Father</p>
+              </div>
+            </div>
+
+            <div className="pt-3">
+              <p className="font-semibold text-[#333333] text-sm">Address</p>
+              <div className="space-y-2">
+                <div className="flex justify-between text-sm">
+                  <p className="text-[#666666]">Address Line 1</p>
+                  <p className="text-[#000000] font-medium">123 Address line 1</p>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <p className="text-[#666666]">Address Line 2</p>
+                  <p className="text-[#000000] font-medium">123 Address line 2</p>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <p className="text-[#666666]">City</p>
+                  <p className="text-[#000000] font-medium">Chennai</p>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <p className="text-[#666666]">State</p>
+                  <p className="text-[#000000] font-medium">Tamil Nadu</p>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <p className="text-[#666666]">Pincode</p>
+                  <p className="text-[#000000] font-medium">600026</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+       <div className="bg-white rounded-[16px]  mb-5 ">
+        <TabNavigation activeTab={activeTab} onTabChange={handleTabChange} />
+      </div>
+      <div className="">
+        {renderTabContent()}
       </div>
     </div>
   );
